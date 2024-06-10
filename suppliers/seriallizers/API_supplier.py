9c7094a928ef607f953b3dba7887c60329c98ef0
Alex <bmgula55@mail.ru>
2024-06-10 09:37:55 +0300

    prohibit the update debt

--- suppliers/seriallizers/API_supplier.py
+++ suppliers/seriallizers/API_supplier.py
@@ -8,6 +8,7 @@ class SupplierAPISerializer(serializers.ModelSerializer):
     author_first_name = serializers.CharField(source="author.first_name", read_only=True)
     author_last_name = serializers.CharField(source="author.last_name", read_only=True)
     product = ProductSerializer(many=True, required=False)
+    read_only_fields = ('debt',)
 
     class Meta:
         model = Supplier
